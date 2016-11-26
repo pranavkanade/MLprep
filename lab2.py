f116b34f3bbb19967e89a6ed4086aa0009d99035
farziCoder <k.pranav.apk@gmail.com>
2016-11-27 03:09:04 +0530

    lab2 tutorial sentdex

---
+++ lab2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import quandl
+
+df = quandl.get('WIKI/GOOGL')
+
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+print(df.head())
+
+#Adj. Close => this wont be our lable as we cannot predict the
+#Adj close value of the next day based on the current one before staritng the trade  
